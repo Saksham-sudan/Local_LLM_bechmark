3195ec14d722829ae1d8450dcb6626d546566060
Saksham-sudan <sudansaksham90@gmail.com>
2026-09-07 17:07:28 +0530

    commit to main

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import csv 
+from ollama import chat
+from pydantic import BaseModel
+
+class schema_structure(BaseModel):
+  thought_process: str
+  answer : str
 
 with open('test_file.csv', newline='') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        print(row)
+        question= row['prompt']
+        response = chat(
+    model='llama3.2:3b',
+    messages=[{'role': 'user', 'content': question}],
+    stream=False,
+    format= schema_structure.model_json_schema()
+)
+
+print(response)
 
