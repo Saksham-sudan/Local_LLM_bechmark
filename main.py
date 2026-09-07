23956d8f25ed84d1341ae3962c2329ab0681221f
Saksham-sudan <sudansaksham90@gmail.com>
2026-09-07 18:14:00 +0530

    final commit of the day

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ class schema_structure(BaseModel):
 with open('test_file.csv', newline='') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        question= row['prompt']
         response = chat(
-    model='llama3.2:3b',
-    messages=[{'role': 'user', 'content': question}],
-    stream=False,
-    format= schema_structure.model_json_schema()
-)
+            model='llama3.2:3b',
+            messages=[{'role': 'user', 'content': row['prompt']}],
+            stream=False,
+            format= schema_structure.model_json_schema(),
+            options= {'temperature': 0},
+        )
+        response_tlm = response.prompt_eval_count
+        response_msg = schema_structure.model_validate_json(response.message.answer)
+        print(response_msg)
+        print(response_tlm)
 
-print(response)
 
