abfd4f2bb4d2c41f86bcb7548d197cffef4f951d
Saksham-sudan <sudansaksham90@gmail.com>
2026-09-18 15:58:10 +0530

    made some structural changes

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import time
 import threading
 import pandas as pd
+import seaborn as sns
 from pynvml import *
 from ollama import chat
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError, Field
 
 class schema_structure(BaseModel):
-  thought_process: str
-  answer : str
+  thought_process: str = Field(
+     description = "Your step-by-step reasoning. Keep it under 15 sentences. DO NOT write the final answer here."
+  )
+  answer : str = Field(
+     description = "The final answer ONLY. You MUST provide a value here. "
+    )
 
 shared_data = {"vram_used": 0}
 data_lock = threading.Lock()
 stop_event = threading.Event()
 
-score_card = {
-   "logic": {"Total": 0, "passed": 0},
-   "math": {"Total": 0, "passed": 0},
-   "knowledge": {"Total": 0, "passed": 0},
-   "coding": {"Total": 0, "passed": 0},
-   "data_extraction": {"Total": 0, "passed": 0},
-}
+result = []
 
 def vram_lookup():
    while not stop_event.is_set() :
@@ -30,19 +29,14 @@ def vram_lookup():
             shared_data["vram_used"] = ram_used
       time.sleep(0.05)
 
-def ans_qual_eval(model_ans, expected_ans, eval_type):
+def ans_qual_eval(model_ans, model_thought, expected_ans,):
    s_model_ans = model_ans.strip().lower()
    s_expected_ans = expected_ans.strip().lower()
-   if eval_type == "exact_match":
-       if s_model_ans == s_expected_ans:
-            return True
-       else:
-            return False
-   elif eval_type == "contains":
-       if s_expected_ans in s_model_ans:
-            return True
-       else:
-            return False
+   s_model_thought = model_thought.strip().lower()
+   if s_expected_ans in s_model_ans or s_expected_ans in s_model_thought:
+      return True
+   else:
+      return False
       
 def benchmark(model_name, prompt_set):
    prompt_data = pd.read_csv(prompt_set, dtype=str)
@@ -51,10 +45,11 @@ def benchmark(model_name, prompt_set):
       with data_lock:
          shared_data['vram_used'] = 0
 
+      print("------------------------------------------------")
       print(f"Processing {row.question_id}: {row.category}...")
       response = chat(
          model=model_name,
-         messages=[{"role": "system", "content": "You are a concise reasoning engine. Think step-by-step"}, {"role": "user", "content": row.prompt}],
+         messages=[{"role": "system", "content": "You are a strict reasoning engine. You must separate your reasoning from your final answer for reasoning you have thought process field. The 'answer' field must contain ONLY the final target value and the answer field must never be empty. Never include explanations in the 'answer' field."}, {"role": "user", "content": row.prompt}],
          stream=False,
          format= schema_structure.model_json_schema(),
          options= {'temperature': 0, 'num_predict': 1024},
@@ -66,33 +61,37 @@ def benchmark(model_name, prompt_set):
       try:
          response_msg = schema_structure.model_validate_json(response.message.content)
       except ValidationError as e:
-         print("Prompt failed")
+         print(f"Prompt failed: {e}")
          continue
+
       ttft_ms = response.prompt_eval_duration/1e6
       dec_token_count = response.eval_count
       dec_time_taken_sec = response.eval_duration/1e9
       tps_sec = dec_token_count/dec_time_taken_sec if dec_time_taken_sec > 0 else 0.0
       total_response_latency_sec = response.total_duration/1e9
+      category = row.category
+      is_correct = ans_qual_eval(response_msg.answer, response_msg.thought_process, row.expected_output)
+
       with data_lock:
          vram_usuage_mb = shared_data['vram_used'] / (1024**2)
-      category = row.category
 
-      print("------------------------------------------------")
       print(row.prompt)
       print(f"Thought Process: {response_msg.thought_process}")
       print(f"answer: {response_msg.answer}")
-      print(ans_qual_eval(response_msg.answer, row.expected_output, row.evaluation_type))
+      print(f"passed: {is_correct}")
       print(f"TTFT in Ms: {ttft_ms}")
       print(f"TPS in /sec: {tps_sec}")
       print(f"Total Response Latency in Sec: {total_response_latency_sec}")
       print(f"Vram Usuage in Mb: {vram_usuage_mb}")
 
-      score_card[category]["Total"] +=1
-      if ans_qual_eval(response_msg.answer, row.expected_output, row.evaluation_type):
-         score_card[category]["passed"] +=1
-      quality = (score_card[category]["passed"] / score_card[category]["Total"] ) * 100
-      print(f"quality: {quality}")
+      result.append({
+         "quesstion_id": row.question_id,
+         "category": row.category,
+         "passed": is_correct
+      })
 
+      data_out = pd.DataFrame(result)
+      data_out.to_csv("test.csv", index = False)
 
 if __name__ == "__main__":
    nvmlInit()
@@ -100,7 +99,7 @@ if __name__ == "__main__":
    monitor_thread.start()
 
    try:
-      benchmark("llama3.2:3b", "standard_prompt_set.csv")
+      benchmark("llama3.2:3b", "prompt_set.csv")
    finally:
       stop_event.set()
       monitor_thread.join()
